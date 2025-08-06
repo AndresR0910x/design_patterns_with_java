51bf2c3111de48e34a2e351d0335339e76d73875
AndresR0910x <androd2000x@gmail.com>
2025-08-05 23:52:16 -0500

    Observer Pattern Learning

---
+++ Observer Pattern/observer-pattern/src/test/java/com/design_patterns/observer_pattern/ObserverPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design_patterns.observer_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
